4ab7a46b1c0f6de4aba21a64eb7d07b03094dfb3
eachebesi <eachebesi@gmail.com>
2022-10-25 21:11:31 -0400

    commit

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "Emmanuel";
-		
+		String name = "vamsi";
+
 		System.out.println(name);
 	}
 
